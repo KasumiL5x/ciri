286bd38bd76d61ec47dea9c5d8a854bc8c7d5363
Daniel Green <KasumiL5x@gmail.com>
2015-08-07 11:40:27 +0100

    Removed forward compatibility from the GL context.  Sticking with a compatibility context over a core context to simplify the GL code, mainly around vertex attrib pointers and lack of need for VAOs in compat.  Also added OpenGL debug output for sanity reasons.

--- ciri/src/ciri/gfx/gl/GLGraphicsDevice.cpp
+++ ciri/src/ciri/gfx/gl/GLGraphicsDevice.cpp
@@ -262,8 +262,6 @@ namespace ciri {
 		for( unsigned int i = 0; i < elements.size(); ++i ) {
 			const VertexElement& currElement = elements[i];
 
-			glEnableVertexAttribArray(i);
-
 			GLenum type;
 			switch( currElement.getFormat() ) {
 				case VertexFormat::Float:
@@ -274,7 +272,9 @@ namespace ciri {
 				}
 			}
 
+			glEnableVertexAttribArray(i);
 			glVertexAttribPointer(i, currElement.getMultiplicity(), type, GL_FALSE, _activeShader->getVertexDeclaration().getStride(), (const void*)offset);
+			
 			offset += currElement.getSize(); // sizeof(datatype) * numberOfThem;
 		}
 
@@ -498,7 +498,7 @@ namespace ciri {
 		}
 
 		// create the newer opengl context
-		int contextFlags = WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB;
+		int contextFlags = 0;// WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB;
 		#ifdef _DEBUG
 			contextFlags |= WGL_CONTEXT_DEBUG_BIT_ARB;
 		#endif
@@ -648,20 +648,63 @@ namespace ciri {
 	}
 
 	void APIENTRY GLGraphicsDevice::debugContextCb( GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam ) {
-		printf("OpenGL Error: %s\n", message);
+		const bool SHOULD_IGNORE_NOTIFICATIONS = true;
+		if( GL_DEBUG_SEVERITY_NOTIFICATION == severity && SHOULD_IGNORE_NOTIFICATIONS ) {
+			return;
+		}
+
 		std::stringstream ss;
-		ss << "OpenGL Error: ";
-		ss << message;
-		ss << "\n";
+		switch( severity ) {
+			case GL_DEBUG_SEVERITY_HIGH: {
+				ss << "OpenGL Error (HIGH): ";
+				break;
+			}
+			case GL_DEBUG_SEVERITY_MEDIUM: {
+				ss << "OpenGL Error (MEDIUM): ";
+				break;
+			}
+			case GL_DEBUG_SEVERITY_LOW: {
+				ss << "OpenGL Error (LOW): ";
+				break;
+			}
+			case GL_DEBUG_SEVERITY_NOTIFICATION: {
+				ss << "OpenGL Notification: ";
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+		ss << message << "\n";
 		OutputDebugString(ss.str().c_str());
+		printf(ss.str().c_str());
 	}
 
 	void APIENTRY GLGraphicsDevice::debugContextAmdCb( GLuint id, GLenum category, GLenum severity, GLsizei length, const GLchar* message, void* userParam ) {
-		printf("OpenGL Error: %s\n", message);
 		std::stringstream ss;
-		ss << "OpenGL Error: ";
-		ss << message;
-		ss << "\n";
+		switch( severity ) {
+			case GL_DEBUG_SEVERITY_HIGH_AMD: {
+					ss << "OpenGL Error (HIGH): ";
+				break;
+			}
+			case GL_DEBUG_SEVERITY_MEDIUM_AMD: {
+					ss << "OpenGL Error (MEDIUM): ";
+				break;
+			}
+			case GL_DEBUG_SEVERITY_LOW_AMD: {
+					ss << "OpenGL Error (LOW): ";
+				break;
+			}
+			case GL_DEBUG_SEVERITY_NOTIFICATION: {
+					ss << "OpenGL Notification: ";
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+		ss << message << "\n";
 		OutputDebugString(ss.str().c_str());
+		printf(ss.str().c_str());
 	}
 } // ciri
