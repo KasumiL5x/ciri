7d6d7d56da1df6bf6bcc9bf026d705b52f7eb753
Daniel Green <KasumiL5x@gmail.com>
2015-08-22 16:16:49 +0100

    Documented ClearFlags.

--- ciri/inc/ciri/gfx/ClearFlags.hpp
+++ ciri/inc/ciri/gfx/ClearFlags.hpp
@@ -2,11 +2,17 @@
 #define __ciri_clearflags__
 
 namespace ciri {
+	/**
+	 * Bitflags for clearing.
+	 */
 	struct ClearFlags {
+		/**
+		 * @see ClearFlags
+		 */
 		enum Flags {
-			Color		=	(1 << 0),
-			Depth		=	(1 << 1),
-			Stencil	=	(1 << 2)
+			Color		=	(1 << 0), /**< Color channel bit. */
+			Depth		=	(1 << 1), /**< Depth channel bit. */
+			Stencil	=	(1 << 2)  /**< Stencil channel bit. */
 		};
 	};
 } // ciri
