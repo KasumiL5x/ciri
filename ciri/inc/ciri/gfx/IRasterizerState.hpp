7577adb9d88b9fbc36d99f8bc2481205a9e1fbb7
Daniel Green <KasumiL5x@gmail.com>
2015-08-22 20:12:20 +0100

    Documented IRasterizerState, and also changed msaa in RasterizerDesc to default to false instead of true.

--- ciri/inc/ciri/gfx/IRasterizerState.hpp
+++ ciri/inc/ciri/gfx/IRasterizerState.hpp
@@ -3,31 +3,37 @@
 
 namespace ciri {
 	struct  FillMode {
+		/**
+		 * Fill mode used when rendering.
+		 */
 		enum Mode {
-			Solid,
-			Wireframe
+			Solid,    /**< Fills triangles formed by vertices. */
+			Wireframe /**< Draws lines connecting vertices. */
 		};
 	};
 
 	struct CullMode {
+		/**
+		 * Triangle winding order cull mode.
+		 */
 		enum Mode {
-			None,
-			Clockwise,
-			CounterClockwise
+			None,            /**< Do not cull triangles regardless of vertex ordering. */
+			Clockwise,       /**< Cull triangles with clockwise vertex ordering. */
+			CounterClockwise /**< Cull triangles with counter-clockwise vertex ordering. */
 		};
 	};
 
 	struct RasterizerDesc {
-		CullMode::Mode cullMode;
-		float depthBias;
-		FillMode::Mode fillMode;
-		bool msaa;
-		bool scissorTestEnable;
-		float slopeScaleDepthBias;
-		bool depthClipEnable;
+		CullMode::Mode cullMode;   /**< Culls certain a vertex winding order. */
+		float depthBias;           /**< Depth value added to a given pixel. */
+		FillMode::Mode fillMode;   /**< Fill mode to use when rendering. */
+		bool msaa;                 /**< Enable or disable multisample antialiasing. */
+		bool scissorTestEnable;    /**< Enable or disable scissor-rectangle culling.  All pixels outside an active scissor rectangle are culled. */
+		float slopeScaleDepthBias; /**< Scalar on a given pixel's slope. */
+		bool depthClipEnable;      /**< Enable or disable clipping based on distance. */
 
 		RasterizerDesc()
-			: cullMode(CullMode::CounterClockwise), depthBias(0.0f), fillMode(FillMode::Solid), msaa(true),
+			: cullMode(CullMode::CounterClockwise), depthBias(0.0f), fillMode(FillMode::Solid), msaa(false),
 				scissorTestEnable(false), slopeScaleDepthBias(0.0f), depthClipEnable(true) {
 		}
 	};
@@ -39,6 +45,9 @@ namespace ciri {
 		virtual ~IRasterizerState() {
 		}
 
+		/**
+		 * Uninitializes the rasterizer state.
+		 */
 		virtual void destroy()=0;
 	};
 } // ciri
