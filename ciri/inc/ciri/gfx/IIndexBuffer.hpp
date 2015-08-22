e23b7e6f7660f8fc50322a743d4881aee04c41a4
Daniel Green <KasumiL5x@gmail.com>
2015-08-22 18:08:54 +0100

    Documented both index buffer and vertex buffer.

--- ciri/inc/ciri/gfx/IIndexBuffer.hpp
+++ ciri/inc/ciri/gfx/IIndexBuffer.hpp
@@ -11,8 +11,24 @@ namespace ciri {
 		virtual ~IIndexBuffer() {
 		}
 
+		/**
+		 * Sets or updates the contents of the index buffer.
+		 * @param indices    Pointer to index array.
+		 * @param indexCount Total number of indices in the array.
+		 * @param dynamic    True if the buffer is dynamic; i.e. will be updated.
+		 * @returns ErrorCode indicating success or failure.
+		 */
 		virtual err::ErrorCode set( int* indices, int indexCount, bool dynamic )=0;
+
+		/**
+		 * Uninitializes the index buffer.
+		 */
 		virtual void destroy()=0;
+
+		/**
+		 * Gets the index count.
+		 * @return Index count provided when data was set.
+		 */
 		virtual int getIndexCount() const=0;
 	};
 } // ciri
--- ciri/inc/ciri/gfx/IVertexBuffer.hpp
+++ ciri/inc/ciri/gfx/IVertexBuffer.hpp
@@ -11,10 +11,31 @@ namespace ciri {
 		virtual ~IVertexBuffer() {
 		}
 
-		// todo: don't require full buffer to be set (right now assumes entire buffer is being set)
+		/**
+		 * Sets or updates the contents of the vertex buffer.
+		 * @param vertices     Pointer to vertex array.
+		 * @param vertexStride Size in bytes of a single vertex.
+		 * @param vertexCount  Total number of vertices in the array.
+		 * @param dynamic      True if the buffer is dynamic; i.e. will be updated.
+		 * @returns ErrorCode indicating success or failure.
+		 */
 		virtual err::ErrorCode set( void* vertices, int vertexStride, int vertexCount, bool dynamic )=0;
+
+		/**
+		 * Uninitializes the vertex buffer.
+		 */
 		virtual void destroy()=0;
+
+		/**
+		 * Gets the size of a single vertex in bytes.
+		 * @return Size of a single vertex in bytes provided when data was set.
+		 */
 		virtual int getStride() const=0;
+
+		/**
+		 * Gets the total number of vertices.
+		 * @return Total number of vertices provided when data was set.
+		 */
 		virtual int getVertexCount()=0;
 	};
 } //ciri
