1ab841e2b73bb14a4a0102f82e078b89645e1b9b
Daniel Green <KasumiL5x@gmail.com>
2015-09-04 16:40:28 +0100

    Added libpng and zlib.

---
+++ ciri/inc/ciri/util/PNG.hpp
@@ -0,0 +1,32 @@
+#ifndef __ciri_png__
+#define __ciri_png__
+
+#pragma comment(lib, "zlib.lib")
+#pragma comment(lib, "libpng16.lib")
+
+namespace ciri {
+	class PNG {
+	public:
+		PNG();
+		~PNG();
+
+		bool loadFromFile( const char* file );
+		void destroy();
+
+		unsigned char* getPixels() const;
+		unsigned int getBytesPerPixel() const;
+		unsigned int getBytesPerChannel() const;
+		unsigned int getWidth() const;
+		unsigned int getHeight() const;
+
+	private:
+		unsigned char* _pixels; // char* ?
+		unsigned int _width;
+		unsigned int _height;
+		unsigned int _bitsPerChannel;
+		unsigned int _bytesPerChannel;
+		unsigned int _channelsPerPixel;
+	};
+}
+
+#endif /* __ciri_png__ */
