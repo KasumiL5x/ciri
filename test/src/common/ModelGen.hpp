578740e8d63891a67e7ced4a28de3ed658015fed
Daniel Green <KasumiL5x@gmail.com>
2015-09-04 19:49:32 +0100

    modelgen's cube can now be created with cw or ccw winding.  modelgen also has a fullscreen quad added (vertex type may change in the future if other ones are added).

--- test/src/common/ModelGen.hpp
+++ test/src/common/ModelGen.hpp
@@ -7,8 +7,46 @@
 
 namespace modelgen {
 	// width; height; depth; u-scale; v-scale
-	static Model* createCube( ciri::IGraphicsDevice* device, float w, float h, float d, float us, float vs ) {
+	static Model* createCube( ciri::IGraphicsDevice* device, float w, float h, float d, float us, float vs, bool ccw=true ) {
 		Model* model = new Model();
+
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, -1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f,  1.0f), cc::Vec3f(), cc::Vec2f()));
+
 		model->addVertex(Vertex(cc::Vec3f(-0.5f*w, -0.5f*h,  0.5f*d), cc::Vec3f( 1.0f,  0.0f,  0.0f), cc::Vec2f(0.0f * us,  0.0f * vs)));
 		model->addVertex(Vertex(cc::Vec3f( 0.5f*w, -0.5f*h,  0.5f*d), cc::Vec3f( 1.0f,  0.0f,  0.0f), cc::Vec2f(1.0f * us,  0.0f * vs)));
 		model->addVertex(Vertex(cc::Vec3f( 0.5f*w,  0.5f*h,  0.5f*d), cc::Vec3f( 1.0f,  0.0f,  0.0f), cc::Vec2f(1.0f * us,  1.0f * vs)));
@@ -34,12 +72,45 @@ namespace modelgen {
 		model->addVertex(Vertex(cc::Vec3f(-0.5f*w,  0.5f*h,  0.5f*d), cc::Vec3f( 0.0f,  0.0f, -1.0f), cc::Vec2f(1.0f * us,  1.0f * vs)));
 		model->addVertex(Vertex(cc::Vec3f(-0.5f*w,  0.5f*h, -0.5f*d), cc::Vec3f( 0.0f,  0.0f, -1.0f), cc::Vec2f(0.0f * us,  1.0f * vs)));
 
-		model->addIndex(0);  model->addIndex(1);  model->addIndex(2);  model->addIndex(0);  model->addIndex(2);  model->addIndex(3);  // +x (right)
-		model->addIndex(4);  model->addIndex(5);  model->addIndex(6);  model->addIndex(4);  model->addIndex(6);  model->addIndex(7);  // -x (left)
-		model->addIndex(8);  model->addIndex(9);  model->addIndex(10); model->addIndex(8);  model->addIndex(10); model->addIndex(11); // +y (top)
-		model->addIndex(12); model->addIndex(13); model->addIndex(14); model->addIndex(12); model->addIndex(14); model->addIndex(15); // -y (bottom)
-		model->addIndex(16); model->addIndex(17); model->addIndex(18); model->addIndex(16); model->addIndex(18); model->addIndex(19); // +z (front)
-		model->addIndex(20); model->addIndex(21); model->addIndex(22); model->addIndex(20); model->addIndex(22); model->addIndex(23); // -z (back)
+		if( ccw ) {
+			model->addIndex(0);  model->addIndex(1);  model->addIndex(2);  model->addIndex(0);  model->addIndex(2);  model->addIndex(3);  // +x (right)
+			model->addIndex(4);  model->addIndex(5);  model->addIndex(6);  model->addIndex(4);  model->addIndex(6);  model->addIndex(7);  // -x (left)
+			model->addIndex(8);  model->addIndex(9);  model->addIndex(10); model->addIndex(8);  model->addIndex(10); model->addIndex(11); // +y (top)
+			model->addIndex(12); model->addIndex(13); model->addIndex(14); model->addIndex(12); model->addIndex(14); model->addIndex(15); // -y (bottom)
+			model->addIndex(16); model->addIndex(17); model->addIndex(18); model->addIndex(16); model->addIndex(18); model->addIndex(19); // +z (front)
+			model->addIndex(20); model->addIndex(21); model->addIndex(22); model->addIndex(20); model->addIndex(22); model->addIndex(23); // -z (back)
+		} else {
+			model->addIndex(2);  model->addIndex(1);  model->addIndex(0);  model->addIndex(3);  model->addIndex(2);  model->addIndex(0);  // +x (right)
+			model->addIndex(6);  model->addIndex(5);  model->addIndex(4);  model->addIndex(7);  model->addIndex(6);  model->addIndex(4);  // -x (left)
+			model->addIndex(10);  model->addIndex(9);  model->addIndex(8); model->addIndex(11);  model->addIndex(10); model->addIndex(8); // +y (top)
+			model->addIndex(14); model->addIndex(13); model->addIndex(12); model->addIndex(15); model->addIndex(14); model->addIndex(12); // -y (bottom)
+			model->addIndex(18); model->addIndex(17); model->addIndex(16); model->addIndex(19); model->addIndex(18); model->addIndex(16); // +z (front)
+			model->addIndex(22); model->addIndex(21); model->addIndex(20); model->addIndex(23); model->addIndex(22); model->addIndex(20); // -z (back)
+		}
+
+		if( !model->build(device) ) {
+			delete model; model = nullptr;
+			return nullptr;
+		}
+		return model;
+	}
+	
+	static Model* createFullscreenQuad( ciri::IGraphicsDevice* device ) {
+		Model* model = new Model();
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		//model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+
+		model->addVertex(Vertex(cc::Vec3f(-1.0f, -1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		model->addVertex(Vertex(cc::Vec3f( 1.0f, -1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		model->addVertex(Vertex(cc::Vec3f( 1.0f,  1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+		model->addVertex(Vertex(cc::Vec3f(-1.0f,  1.0f, 0.0f), cc::Vec3f(), cc::Vec2f()));
+
+		model->addIndex(0); model->addIndex(1); model->addIndex(3);
+		model->addIndex(3); model->addIndex(1); model->addIndex(2);
 
 		if( !model->build(device) ) {
 			delete model; model = nullptr;
