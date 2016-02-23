f87177fdfaed7b60e838d6cd29016f4d600dab84
Daniel Green <KasumiL5x@gmail.com>
2016-02-23 18:36:45 +0000

    Added a Leb128 decoder.

---
+++ test/src/common/Leb128.hpp
@@ -0,0 +1,50 @@
+#ifndef __leb128__
+#define __leb128__
+
+#include <fstream>
+
+namespace leb128
+{
+	/**
+		* Decodes a signed integer encoded with LEB128-encoded from a stream.
+		* Assumes the position in the stream is set and the data is valid.
+		*
+		* http://en.wikipedia.org/wiki/LEB128
+		*/
+	static int decodeStream( std::ifstream& data, int* outSize=nullptr )
+	{
+		int result = 0;
+		int shift = 0;
+		int size = 0;
+		while( true )
+		{
+			// Read next byte.
+			int b;
+			data.read(reinterpret_cast<char*>(&b), sizeof(unsigned char));
+
+			// Increment size.
+			size += 1;
+
+			// Or with low order 7 bits of the byte
+			result |= (b & 0x7f) << shift;
+
+			// If the high order of the bit is zero, we're done
+			if( (b & 0x80) == 0 )
+			{
+				break;
+			}
+
+			// Shift by 7 bytes
+			shift += 7;
+		}
+
+		if( outSize )
+		{
+			*outSize = size;
+		}
+
+		return result;
+	}
+}
+
+#endif /* __leb128__ */
