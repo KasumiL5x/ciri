9babd5f66d60d85d910b6765aeeac1faf5d4eb63
Daniel Green <KasumiL5x@gmail.com>
2016-05-16 08:06:02 +0100

    Added Leb128 decoder to Core.

---
+++ ciri/inc/ciri/core/Leb128.hpp
@@ -0,0 +1,45 @@
+#ifndef __ciri_core_leb128__
+#define __ciri_core_leb128__
+
+namespace ciri {
+	namespace leb128 {
+	/**
+		* Decodes a signed integer encoded with LEB128-encoded from a stream.
+		* Assumes the position in the stream is set and the data is valid.
+		*
+		* http://en.wikipedia.org/wiki/LEB128
+		*/
+		static int decodeStream( std::ifstream& data, int* outSize=nullptr ) {
+			int result = 0;
+			int shift = 0;
+			int size = 0;
+			while( true ) {
+				// Read next byte.
+				int b;
+				data.read(reinterpret_cast<char*>(&b), sizeof(unsigned char));
+
+				// Increment size.
+				size += 1;
+
+				// Or with low order 7 bits of the byte
+				result |= (b & 0x7f) << shift;
+
+				// If the high order of the bit is zero, we're done
+				if( (b & 0x80) == 0 ) {
+					break;
+				}
+
+				// Shift by 7 bytes
+				shift += 7;
+			}
+
+			if( outSize ) {
+				*outSize = size;
+			}
+
+			return result;
+		}
+	} // leb128
+} // ciri
+
+#endif /* __ciri_core_leb128__ */
